786990a45b1da8107c8e4421c853d644884d964f
calvinProbst <20calvin.probst@gmail.com>
2019-09-11 13:11:24 -0600

    having issues

--- minesweeper.py
+++ minesweeper.py
@@ -6,17 +6,20 @@
 import random
 import pygame
 import sys
+import time
 from pygame.locals import *
 
 FPS = 30
 BOARDWIDTH = 20
 BOARDHEIGHT = 20
-WINDOWWIDTH = 0
-WINDOWHEIGHT = 0
+BOXSIZE = 20
+GAPSIZE = 3
+WINDOWWIDTH = BOXSIZE * BOARDWIDTH + GAPSIZE * (BOARDWIDTH + 1)
+WINDOWHEIGHT = BOXSIZE * BOARDHEIGHT + GAPSIZE * (BOARDHEIGHT + 1)
 EXPLOSIONSPEED = 10
-BOMBCOUNT = 60
+BOMBCOUNT = int((BOARDHEIGHT * BOARDWIDTH) * 3 / 20)
 
-assert BOMBCOUNT > BOARDHEIGHT * BOARDWIDTH, "Bomb count is larger than square count"
+assert BOMBCOUNT < BOARDHEIGHT * BOARDWIDTH, "Bomb count is larger than square count"
 
 # Colors
 RED = (255, 0, 0)
@@ -25,35 +28,30 @@ GREEN = (0, 255, 0)
 WHITE = (255, 255, 255)
 
 BGCOLOR = RED
+print("all vars init")
+pygame.init()
+pygame.mixer_music.load("Alcazar.mp3")
+pygame.mixer_music.play(-1, 0.0)
+print("1")
+FPSCLOCK = pygame.time.Clock()
+DISPLAYSURF = pygame.display.set_mode((WINDOWWIDTH, WINDOWHEIGHT))
 
-
-# noinspection PyGlobalUndefined
 def main():
-    global FPSCLOCK
-    global DISPLAYSURF
-    pygame.init()
-    pygame.mixer_music.load("Alcazar.mp3")
-    pygame.mixer_music.play(-1, 0.0)
-
-    FPSCLOCK = pygame.time.Clock()
-    DISPLAYSURF = pygame.display.set_mode((WINDOWWIDTH, WINDOWHEIGHT))
 
     pygame.display.set_caption("Minesweeper")
 
     mousex = 0
     mousey = 0
-
+    print("2")
     gameBoard = makeNewBoard(BOARDWIDTH, BOARDHEIGHT)
-
     flags = [[False] * BOARDHEIGHT] * BOARDWIDTH
 
-    firstSelection = None
 
     while True:
         mouseClicked = False
 
         drawGameBoard(flags)
-
+        print("2")
         for event in pygame.event.get():
             if event.type == QUIT or (event.type == KEYUP and event.key == K_ESCAPE):
                 pygame.quit()
@@ -64,9 +62,9 @@ def main():
                 mousex, mousey = event.pos
                 mouseClicked = True
 
-        box = getBoxPos(mousex, mousey)
+        boxX, boxY = getBoxPos(mousex, mousey)
         if mouseClicked:
-            print("click" + str(box))
+            print("click" + str(boxX, boxY))
 
 
 def makeNewBoard(width, height):
@@ -82,16 +80,26 @@ def makeNewBoard(width, height):
 
 
 def getBoxPos(x, y):
-    return x, y
+    boxXOverlap = (x - GAPSIZE) % (BOXSIZE + GAPSIZE)
+    boxXPos = (x - GAPSIZE - boxXOverlap) / (BOXSIZE + GAPSIZE)
+    if not boxXOverlap < BOXSIZE:
+        boxXPos = -1
+    boxYOverlap = (y - GAPSIZE) % (BOXSIZE + GAPSIZE)
+    boxYPos = (y - GAPSIZE - boxYOverlap) / (BOXSIZE + GAPSIZE)
+    if not boxYOverlap < BOXSIZE:
+        boxYPos = -1
+    return boxXPos, boxYPos
 
 
 def drawGameBoard(flagBoard):
     DISPLAYSURF.fill(BGCOLOR)
-    for x in flagBoard:
-        for y in flagBoard[x]:
+    for x in range(BOARDWIDTH):
+        for y in range(BOARDHEIGHT):
             # Draw a rectangle for the box
             # fix with math
-            pygame.draw.rect(DISPLAYSURF, GREEN, (6, 7, 8, 9))
-            if flagBoard[x][y]:
-                # Replace with drawing actual flag at position
-                pygame.draw.polygon(DISPLAYSURF, RED, ((1, 0), (1, 1), (2, 2)))
+            pygame.draw.rect(DISPLAYSURF, WHITE, ((GAPSIZE + (GAPSIZE + BOXSIZE) * x), (GAPSIZE + (GAPSIZE + BOXSIZE) * y), BOXSIZE, BOXSIZE))
+            # if flagBoard[x][y]:
+            # Replace with drawing actual flag at position
+            # pygame.draw.polygon(DISPLAYSURF, RED, ((1, 0), (1, 1), (2, 2)))
+
+main()
